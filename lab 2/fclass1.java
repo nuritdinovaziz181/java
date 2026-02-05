ff9285ef7a92f9d9382a2cc051528a577efa4905
nuritdinovaziz181 <nuritdinovaziz181@gmail.com>
2026-02-06 00:20:15 +0500

    Refactor fclass1 and fclass2 methods

--- lab 2/fclass1.java
+++ lab 2/fclass1.java
@@ -1,24 +1,21 @@
 class fclass1 {
-    public void show1() {
-        System.out.println("Бұл fclass1 класы");
-    }
+    int a, b;
 
     public static void main(String[] args) {
         fclass2 obj = new fclass2();
-        obj.show1();
-        obj.show3();
-        obj.show2();
+        obj.input();
+        obj.show();
     }
 }
 
-class fclass3 extends fclass1 {
-    public void show3() {
-        System.out.println("Бұл fclass3 класы");
+class fclass2 extends fclass1 {
+    void input() {
+        a = 10;
+        b = 20;
     }
-}
 
-class fclass2 extends fclass3 {
-    public void show2() {
-        System.out.println("Бұл fclass2 класы");
+    void show() {
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
